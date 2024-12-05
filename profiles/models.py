0e895124c4f6a709797ac7515ee82fd61a62f8f1
Bilal Essafi <3trn873j@students.codeinstitute.net>
2024-12-05 18:07:15 +0000

    Update profile default image

--- profiles/models.py
+++ profiles/models.py
@@ -12,7 +12,7 @@ class Profile(models.Model):
     name = models.CharField(max_length=255, blank=True)
     content = models.TextField(blank=True)
     image = models.ImageField(
-        upload_to='images/', default='../coffee'
+        upload_to='images/', default='../green-apple_kupzt9'
     )
 
     class Meta:
