4acc1b3989819fb7ad603e847acd91e75bd2e8d2
Bilal Essafi <3trn873j@students.codeinstitute.net>
2024-12-05 09:44:35 +0000

    Add count fields to allow to illustrate profile statistics

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,11 +1,10 @@
-from rest_framework import serializers
-from .models import Profile
-from followers.models import Follower
-
-class ProfileSerializer(serializers.ModelSerializer):
+Copyclass ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     following_id = serializers.SerializerMethodField()
+    posts_count = serializers.SerializerMethodField()
+    followers_count = serializers.SerializerMethodField()
+    following_count = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
@@ -20,9 +19,19 @@ class ProfileSerializer(serializers.ModelSerializer):
             return following.id if following else None
         return None
 
+    def get_posts_count(self, obj):
+        return obj.owner.post_set.count()
+
+    def get_followers_count(self, obj):
+        return Follower.objects.filter(followed=obj.owner).count()
+
+    def get_following_count(self, obj):
+        return Follower.objects.filter(owner=obj.owner).count()
+
     class Meta:
         model = Profile
         fields = [
             'id', 'owner', 'created_at', 'updated_at', 'name',
             'content', 'image', 'is_owner', 'following_id',
+            'posts_count', 'followers_count', 'following_count'
         ]
