c52a0f0dbf1c5871b37602905fa8799c62ecde80
Bilal Essafi <3trn873j@students.codeinstitute.net>
2024-12-08 21:01:39 +0000

    Fix dj-auth bug to allow for proper logout

--- drf_api/views.py
+++ drf_api/views.py
@@ -13,7 +13,7 @@ def root_route(request):
     })
 
 # dj-rest-auth logout view fix
-def logout_foute(request):
+def logout_route(request):
     response = Response()
     response.set_cookie(
         key=JWT_AUTH_COOKIE,
