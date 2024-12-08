7f5a39e61aa47c6a594f5a217fb0e9e5915a7f5f
Bilal Essafi <3trn873j@students.codeinstitute.net>
2024-12-08 19:54:46 +0000

    Set Debug to true for testing

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -69,7 +69,7 @@ if not SECRET_KEY:
     raise ValueError("The SECRET_KEY environment variable is not set!")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '8000-bilalessafi1-drfapi-3ety8hg1ccw.ws.codeinstitute-ide.net',
