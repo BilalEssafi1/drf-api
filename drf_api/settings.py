8fb12ec56d1d3ffb0cf3a57b976d43ba4f3c7ef7
Bilal Essafi <3trn873j@students.codeinstitute.net>
2024-12-09 09:00:09 +0000

    Update CORS settings to debug login issue

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -141,6 +141,27 @@ if 'CLIENT_ORIGIN_DEV' in os.environ:
 
 CORS_ALLOW_CREDENTIALS = True
 
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+
 ROOT_URLCONF = 'drf_api.urls'
 
 TEMPLATES = [
