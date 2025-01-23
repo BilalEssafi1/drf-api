a210e8a07e4b63abf13aafa3e53aaec70e6311fd
Bilal Essafi <3trn873j@students.codeinstitute.net>
2025-01-23 20:57:14 +0000

    add CustomReigsterView with cookie cleanup for registration

--- drf_api/registration.py
+++ drf_api/registration.py
@@ -2,30 +2,44 @@ from dj_rest_auth.registration.views import RegisterView
 from rest_framework.response import Response
 from django.middleware.csrf import rotate_token
 from django.conf import settings
+from rest_framework import status
+
+JWT_AUTH_COOKIE = getattr(settings, 'JWT_AUTH_COOKIE', None)
+JWT_AUTH_REFRESH_COOKIE = getattr(settings, 'JWT_AUTH_REFRESH_COOKIE', None)
+JWT_AUTH_SAMESITE = getattr(settings, 'JWT_AUTH_SAMESITE', 'None')
+JWT_AUTH_SECURE = getattr(settings, 'JWT_AUTH_SECURE', True)
 
 class CustomRegisterView(RegisterView):
-    def perform_create(self, serializer):
-        user = super().perform_create(serializer)
-        response = Response({'detail': 'Registered successfully'})
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
         
+        response = Response(
+            {'detail': 'Registered successfully'},
+            status=status.HTTP_201_CREATED,
+            headers=headers
+        )
+
         # Clear JWT auth cookies
         response.set_cookie(
-            key=settings.JWT_AUTH_COOKIE,
+            key=JWT_AUTH_COOKIE,
             value='',
             httponly=True,
             expires='Thu, 01 Jan 1970 00:00:00 GMT',
             max_age=0,
-            samesite=settings.JWT_AUTH_SAMESITE,
-            secure=settings.JWT_AUTH_SECURE,
+            samesite=JWT_AUTH_SAMESITE,
+            secure=JWT_AUTH_SECURE,
         )
         response.set_cookie(
-            key=settings.JWT_AUTH_REFRESH_COOKIE,
+            key=JWT_AUTH_REFRESH_COOKIE,
             value='',
             httponly=True,
             expires='Thu, 01 Jan 1970 00:00:00 GMT',
             max_age=0,
-            samesite=settings.JWT_AUTH_SAMESITE,
-            secure=settings.JWT_AUTH_SECURE,
+            samesite=JWT_AUTH_SAMESITE,
+            secure=JWT_AUTH_SECURE,
         )
 
         # Clear all auth cookies
@@ -42,5 +56,5 @@ class CustomRegisterView(RegisterView):
                 path='/'
             )
 
-        rotate_token(self.request)
+        rotate_token(request)
         return response
