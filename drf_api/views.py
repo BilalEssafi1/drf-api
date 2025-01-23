90ebf4bf9bcb58ad488beb269c18f6adecd6c256
Bilal Essafi <3trn873j@students.codeinstitute.net>
2025-01-23 15:33:08 +0000

    enhance session cleanup and CSRF token rotation in API logout endpoint

--- drf_api/views.py
+++ drf_api/views.py
@@ -1,60 +1,64 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.middleware.csrf import rotate_token
 from .settings import (
-   JWT_AUTH_COOKIE, JWT_AUTH_REFRESH_COOKIE, JWT_AUTH_SAMESITE,
-   JWT_AUTH_SECURE,
+    JWT_AUTH_COOKIE, JWT_AUTH_REFRESH_COOKIE, JWT_AUTH_SAMESITE,
+    JWT_AUTH_SECURE,
 )
-
 
 @api_view()
 def root_route(request):
-   return Response({
-       "message": "Welcome to my drf API!"
-   })
+    """
+    Root route view
+    Returns welcome message for API
+    """
+    return Response({
+        "message": "Welcome to my drf API!"
+    })
 
-
-# dj-rest-auth logout view fix
 @api_view(['POST'])
 def logout_route(request):
-   response = Response()
-   response.set_cookie(
-       key=JWT_AUTH_COOKIE,
-       value='',
-       httponly=True,
-       expires='Thu, 01 Jan 1970 00:00:00 GMT',
-       max_age=0,
-       samesite=JWT_AUTH_SAMESITE,
-       secure=JWT_AUTH_SECURE,
-   )
-   response.set_cookie(
-       key=JWT_AUTH_REFRESH_COOKIE,
-       value='',
-       httponly=True,
-       expires='Thu, 01 Jan 1970 00:00:00 GMT',
-       max_age=0,
-       samesite=JWT_AUTH_SAMESITE,
-       secure=JWT_AUTH_SECURE,
-   )
-    # Clear the csrftoken and sessionid cookies
-   response.set_cookie(
-       key='csrftoken',
-       value='',
-       httponly=False,
-       expires='Thu, 01 Jan 1970 00:00:00 GMT',
-       max_age=0,
-       samesite='None',
-       secure=True,
-       path='/'
-   )
-   response.set_cookie(
-       key='sessionid',
-       value='',
-       httponly=False,
-       expires='Thu, 01 Jan 1970 00:00:00 GMT',
-       max_age=0,
-       samesite='None',
-       secure=True,
-       path='/'
-   )
+    """
+    Custom logout view
+    Clears auth cookies and rotates CSRF token
+    """
+    response = Response()
+    
+    # Clear JWT auth cookies
+    response.set_cookie(
+        key=JWT_AUTH_COOKIE,
+        value='',
+        httponly=True,
+        expires='Thu, 01 Jan 1970 00:00:00 GMT',
+        max_age=0,
+        samesite=JWT_AUTH_SAMESITE,
+        secure=JWT_AUTH_SECURE,
+    )
+    response.set_cookie(
+        key=JWT_AUTH_REFRESH_COOKIE,
+        value='',
+        httponly=True,
+        expires='Thu, 01 Jan 1970 00:00:00 GMT',
+        max_age=0,
+        samesite=JWT_AUTH_SAMESITE,
+        secure=JWT_AUTH_SECURE,
+    )
 
-   return response
+    # Clear session and CSRF cookies
+    cookies_to_clear = ['csrftoken', 'sessionid', 'messages', 'my-app-auth', 'my-refresh-token']
+    for cookie in cookies_to_clear:
+        response.set_cookie(
+            key=cookie,
+            value='',
+            httponly=True,
+            expires='Thu, 01 Jan 1970 00:00:00 GMT',
+            max_age=0,
+            samesite='None',
+            secure=True,
+            path='/'
+        )
+
+    # Rotate CSRF token for security
+    rotate_token(request)
+    
+    return response
