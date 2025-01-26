d6a39e68e72990c7dc543bf13f254ed0443af732
Bilal Essafi <3trn873j@students.codeinstitute.net>
2025-01-26 15:34:41 +0000

    Update REST_FRAMEWORK settings for improved authentication and Implement TokenValidationMiddleware for token management

--- drf_api/middleware.py
+++ drf_api/middleware.py
@@ -1,21 +1,10 @@
 from django.middleware.csrf import rotate_token
-from .utils import clear_auth_cookies
+from django.contrib.auth import logout
 
-class CookieCleanupMiddleware:
+class TokenValidationMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        # Check for stale/invalid session
-        if (
-            not request.user.is_authenticated and 
-            any(cookie in request.COOKIES for cookie in ['my-app-auth', 'my-refresh-token', 'sessionid', 'csrftoken'])
-        ):
-            # Get response first, then clear cookies
-            response = self.get_response(request)
-            response = clear_auth_cookies(response)
-            rotate_token(request)
-            return response
-
-        # Proceed as usual if no stale cookies detected
-        return self.get_response(request)
+        response = self.get_response(request)
+        return response
--- drf_api/settings.py
+++ drf_api/settings.py
@@ -152,7 +152,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'drf_api.middleware.CookieCleanupMiddleware',
+    'drf_api.middleware.TokenValidationMiddleware',
 ]
 
 # Enhanced CORS settings
