836bdcbc19638ba444fa01d6d5a0cefdc1f95b57
Bilal Essafi <3trn873j@students.codeinstitute.net>
2025-01-26 11:47:39 +0000

    Fix stale cookie issues by enhancing middleware by moving it earlier in the middleware stack and clear all relevant cookies

--- drf_api/middleware.py
+++ drf_api/middleware.py
@@ -7,9 +7,15 @@ class CookieCleanupMiddleware:
 
     def __call__(self, request):
         # Check for stale/invalid session
-        if not request.user.is_authenticated and 'my-app-auth' in request.COOKIES:
+        if (
+            not request.user.is_authenticated and 
+            any(cookie in request.COOKIES for cookie in ['my-app-auth', 'my-refresh-token', 'sessionid', 'csrftoken'])
+        ):
+            # Get response first, then clear cookies
             response = self.get_response(request)
             response = clear_auth_cookies(response)
             rotate_token(request)
             return response
+
+        # Proceed as usual if no stale cookies detected
         return self.get_response(request)
